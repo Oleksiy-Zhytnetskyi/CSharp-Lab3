cae38c5f7bad3a68dbaf10e9012afbe3e6e17f5d
Oleksiy Zhytnetskyi <o.zhytnetskyi@ukma.edu.ua>
2024-04-07 00:31:59 +0300

    Initial commit

---
+++ Lab3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace KMA.CSharp2024.Lab3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Lab3/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace KMA.CSharp2024.Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
